9c85c97e8f57c8bc939584c9428a814e8db2a685
mgjo5899 <mgjo5899@gmail.com>
2018-10-22 03:09:07 -0500

    Adjusting schema to reflect the API doc

--- backend/raw_queries.py
+++ backend/raw_queries.py
@@ -2,23 +2,44 @@
 
 create_users = """
                CREATE TABLE Users (
-                   email VARCHAR(256) NOT NULL,
+                   id INT NOT NULL,
+                   email VARCHAR(256) NOT NULL UNIQUE,
                    username VARCHAR(256) NOT NULL UNIQUE,
                    password VARCHAR(256) NOT NULL,
                    last_login DATETIME NOT NULL,
-                   PRIMARY KEY (email)
+                   PRIMARY KEY (id)
                )
                """
 create_accounts = """
                   CREATE TABLE Accounts (
+                      id INT NOT NULL UNIQUE,
+                      user_id INT NOT NULL,
                       account_name VARCHAR(256) NOT NULL,
-                      email VARCHAR(256) NOT NULL,
                       open_date DATETIME NOT NULL,
                       close_date DATETIME,
-                      PRIMARY KEY (account_name, email),
-                      FOREIGN KEY (email) REFERENCES Users(email) ON DELETE CASCADE
+                      PRIMARY KEY (user_id, account_name),
+                      FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
                   )
                   """
+create_positions = """
+                   CREATE TABLE Positions (
+                       id INT NOT NULL UNIQUE,
+                       account_id INT NOT NULL,
+                       open_rate_id INT NOT NULL,
+                       close_rate_id INT,
+                       position_type VARCHAR(256) NOT NULL,
+                       position_status VARCHAR(256) NOT NULL,
+                       volume FLOAT NOT NULL,
+                       PRIMARY KEY (account_id, id),
+                       FOREIGN KEY (account_id)
+                           REFERENCES Accounts(id)
+                           ON DELETE CASCADE,
+                       FOREIGN KEY (open_rate_id)
+                           REFERENCES ExchangeRates(id),
+                       FOREIGN KEY (close_rate_id)
+                           REFERENCES ExchangeRates(id)
+                   )
+                   """
 create_exchangerates = """
                        CREATE TABLE ExchangeRates (
                            id INT NOT NULL AUTO_INCREMENT,
@@ -30,22 +51,3 @@ create_exchangerates = """
                            PRIMARY KEY (id)
                        )
                        """
-create_positions = """
-                   CREATE TABLE Positions (
-                       account_name VARCHAR(256) NOT NULL,
-                       position_id INT NOT NULL,
-                       open_rate_id INT NOT NULL,
-                       close_rate_id INT,
-                       position_type VARCHAR(256) NOT NULL,
-                       position_status VARCHAR(256) NOT NULL,
-                       volume FLOAT NOT NULL,
-                       PRIMARY KEY (account_name, position_id),
-                       FOREIGN KEY (account_name)
-                           REFERENCES Accounts(account_name)
-                           ON DELETE CASCADE,
-                       FOREIGN KEY (open_rate_id)
-                           REFERENCES ExchangeRates(id),
-                       FOREIGN KEY (close_rate_id)
-                           REFERENCES ExchangeRates(id)
-                   )
-                   """
