24acfa97e0140b3f9dfa0ab35cf4d60142587655
bkille <kille2@illinois.edu>
2018-10-31 16:16:07 -0500

    account and user tests

--- backend/tests/test_manage.py
+++ backend/tests/test_manage.py
@@ -6,26 +6,83 @@ sys.path.extend(['', '/home/bryce/classes/CS411/Traders/backend/tests', '/home/b
 from traders_back import manage
 
 def test_users():
-    test_email = "test_email@email.com"
-    test_name = "testuname"
-    test_pass = "testpass"
+    for i in range(10):
+        test_email = "test_email@email.com" + str(i)
+        test_name = "testuname" + str(i)
+        test_pass = "testpass" + str(i)
+        delete_response = manage.delete_user(test_email, test_pass)
+        
+    test_email = "test_email@email.com0"
+    test_name = "testuname0"
+    test_pass = "testpass0"
     old_users = manage.get_all_users()['users']
     signup_response = manage.sign_up(test_email, test_name, test_pass)
     uid = signup_response['user_id']
-    new_users = manage.get_all_users()['users']
+    
+    error = None
     try:
-        assert len(old_users) == len(new_users) - 1
-    except AssertionError:
-        delete_response = manage.delete_user(test_email, test_pass)
-        raise
-    delete_response = manage.delete_user(test_email, test_pass)
+        for i in range(10):
+            test_email = "test_email@email.com" + str(i)
+            test_name = "testuname" + str(i)
+            test_pass = "testpass" + str(i)
+            signup_response = manage.sign_up(test_email, test_name, test_pass)
+            if i == 0:
+                assert not signup_response['status'] 
+            else:
+                assert signup_response['status']
+        
+            new_users = manage.get_all_users()['users']
+        assert len(old_users) == len(new_users) - 10
+    except Exception as e: 
+        error = e
+        pass #Change to raise if you want to see where the error occured 
     
     for i in range(10):
         test_email = "test_email@email.com" + str(i)
         test_name = "testuname" + str(i)
         test_pass = "testpass" + str(i)
+        delete_response = manage.delete_user(test_email, test_pass)
+        
+    if error is not None:
+        raise error
+        
+def test_accounts():
+    try:
+        test_email = "test_email@email.com0"
+        test_name = "testuname0"
+        test_pass = "testpass0"
         signup_response = manage.sign_up(test_email, test_name, test_pass)
+        uid = signup_response['user_id']
+        user_initial_accounts = manage.get_user_accounts(uid)
+        assert len(user_initial_accounts['accounts']) == 0 #MAY CHANGE if we want users to initally have an account
         
+        test0_id = manage.create_account(uid, "test_acc0")['account_id']
+        user_accounts = manage.get_user_accounts(uid)
+        assert len(user_accounts['accounts']) == 1
         
+        dup_account_status = manage.create_account(uid, "test_acc0")['status']
+        assert not dup_account_status
+        
+        test1_id = manage.create_account(uid, "test_acc1")['account_id']
+        user_accounts = manage.get_user_accounts(uid)
+        assert len(user_accounts['accounts']) == 2
+        
+        delete_results = manage.delete_account(uid, test1_id)
+        missing_acc_info = manage.get_account_info(uid, test1_id)
+        assert not missing_acc_info['status']
+        
+        user_accounts = manage.get_user_accounts(uid)
+        acc1_info = manage.get_account_info(uid, test0_id)
+        
+        assert acc1_info['account_info'] == user_accounts["accounts"][0]
+        
+        delete_response = manage.delete_user(test_email, test_pass)
+    except:
+        delete_response = manage.delete_user(test_email, test_pass)
+        raise
+        
+    
+       
 test_users()
+test_accounts()
     
