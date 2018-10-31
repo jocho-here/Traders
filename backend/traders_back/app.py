e662edaa5decd19fe1a0ffa245d606dc4dddf486
mgjo5899 <mgjo5899@gmail.com>
2018-10-31 12:43:28 -0500

    Registering positions and exchange_rates to the app blueprints

--- backend/traders_back/app.py
+++ backend/traders_back/app.py
@@ -4,6 +4,8 @@ from flask_cors import CORS
 from utils import check_tables
 from views.accounts import accounts
 from views.users import users
+from views.exchange_rates import exchange_rates
+from views.positions import positions
 
 # This is where we start our backend app
 app = Flask(__name__)
@@ -11,6 +13,8 @@ app = Flask(__name__)
 # Blueprint registration
 app.register_blueprint(accounts)
 app.register_blueprint(users)
+app.register_blueprint(exchange_rates)
+app.register_blueprint(positions)
 
 CORS(app=app)
 
