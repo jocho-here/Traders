e166243b0384b69f21079b892023c69da0944225
bkille <kille2@illinois.edu>
2018-10-31 18:38:42 -0500

    Tests for users, accounts, exchanges and positions

--- backend/tests/test_manage.py
+++ backend/tests/test_manage.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from datetime import datetime, timedelta
+import time
 sys.path.extend(['', '/home/bryce/classes/CS411/Traders/backend/tests', '/home/bryce/classes/CS411/Traders/backend/dist/traders_back-0.0.1-py3.6.egg', '/home/bryce/.local/share/virtualenvs/backend-UR8qZ0SU/lib/python36.zip', '/home/bryce/.local/share/virtualenvs/backend-UR8qZ0SU/lib/python3.6', '/home/bryce/.local/share/virtualenvs/backend-UR8qZ0SU/lib/python3.6/lib-dynload', '/home/bryce/miniconda3/envs/411/lib/python3.6', '/home/bryce/.local/share/virtualenvs/backend-UR8qZ0SU/lib/python3.6/site-packages', '/home/bryce/classes/CS411/Traders/backend'])
 
 from traders_back import manage
@@ -80,9 +82,75 @@ def test_accounts():
     except:
         delete_response = manage.delete_user(test_email, test_pass)
         raise
+     
+def test_exchange_rates():
+    try:
+        nowish = (datetime.now() - timedelta(days=10)).replace(microsecond=0)
+        starttime = nowish - timedelta(days=1)
+        endtime = nowish + timedelta(days=1)
+        topair = "GBP"
+        frompair = "USD"
+        get_exch_status = manage.get_exchange_rates(frompair, topair, from_time=starttime, to_time=endtime)
+        old_exch_rates = get_exch_status['exchange_rates']
+        now = nowish
+        for i in range(10):
+            for frompair in ["USD", "AUD", "CAD"]:
+                for topair in ["GBP", "EUR", "JPY"]:
+                    now = (now + timedelta(seconds=2)).replace(microsecond=0)
+                    assert now > starttime
+                    assert now < endtime
+                    bid = 0.8 +i
+                    ask = 0.7 + i
+                    manage.add_exchange_rate(frompair, topair, bid, ask, now)
+                    get_exch_status = manage.get_exchange_rates(frompair, topair, time=now)
+                    get_exch_status['exchange_rate'].pop('id', None)
+                    assert get_exch_status['currency_from'] == frompair
+                    assert get_exch_status['currency_to'] == topair
+                    assert get_exch_status['exchange_rate'] == {'bid': bid, 'ask': ask, 'time': now}
+
+        get_exch_status = manage.get_exchange_rates(frompair, topair, from_time=starttime, to_time=endtime)
+        print(len(get_exch_status['exchange_rates']))
+        print(len(old_exch_rates))
+        assert len(get_exch_status['exchange_rates']) - len(old_exch_rates) == 10
+    except:
+        raise
         
     
+def test_positions():
+    try:
+        test_email = "test_email@email.com0"
+        test_name = "testuname0"
+        test_pass = "testpass0"
+        signup_response = manage.sign_up(test_email, test_name, test_pass)
+        uid = signup_response['user_id']
+        test0_id = manage.create_account(uid, "test_acc0")['account_id']
+        user_accounts = manage.get_user_accounts(uid)
+        get_positions_response = manage.get_positions(test0_id)
+        assert len(get_positions_response['positions']) == 0
+        
+        frompair = "USD"
+        topair = "GBP"
+        now = datetime.now().replace(microsecond=0)
+        manage.add_exchange_rate(frompair, topair, 1, .75, now)
+        pos_id = manage.create_position(test0_id, frompair, topair, now, position_type='long', volume=10)['position_id']
+        get_positions_response = manage.get_positions(test0_id)
+        assert len(get_positions_response['positions']) == 1
+        
+        now += timedelta(seconds=10)
+        rate_id = manage.add_exchange_rate(frompair, topair, 2, 1.5, now)['rate_id']
+        
+        manage.close_position(pos_id, rate_id)
+        get_positions_response = manage.get_positions(test0_id)
+        position = get_positions_response['positions'][0]
+        print(position)
+        assert position
+        
+    except:
+        delete_response = manage.delete_user(test_email, test_pass)
+        raise
        
 test_users()
 test_accounts()
+test_exchange_rates()
+test_positions()
     
